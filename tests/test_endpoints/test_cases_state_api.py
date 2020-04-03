224c3a8bccbb883b49ac002223b4da7c80397a04
evertoncastro <evertoncastro.sp@gmail.com>
2020-04-02 23:22:58 -0300

    New endpoint for cases per day

--- tests/test_endpoints/test_cases_state_api.py
+++ tests/test_endpoints/test_cases_state_api.py
@@ -1,7 +1,8 @@
 import json
+import datetime
 from unittest import TestCase
 from app import app, db
-from models import City, CasesLocation, State, StateCases
+from models import State, StateCases, StateCasesPerDay
 from tests.runner import clear_db
 
 
@@ -51,6 +52,42 @@ class TestDataApi(TestCase):
             'totalCases': '6',
             'totalCasesMS': '4',
             'deaths': '8'
+        })
+
+    def test_endpoint_list_state_cases_daily(self):
+        # Seed test data
+        State().save(self.db.session, abbreviation='SP',
+                     name='São Paulo',
+                     lat=12.0001, lng=25.0001)
+        StateCasesPerDay().save(self.db.session, id=1, date=datetime.date(2020, 3, 29),
+                                country='Brazil', state_id=1, newcases=3, totalcases=35)
+        self.db.session.commit()
+        resp = self.client.get(
+            '/data_api/v1/cases/state/daily/1',
+            headers={
+                'Authorization': f"Bearer {self.authentication['access_token']}"
+            }
+        )
+        result = json.loads(resp.get_data(as_text=True))
+        self.assertEqual(result, {"cases": [{
+                 "stateCode": "SP",
+                 "stateName": "São Paulo",
+                 "lat": "12.0001",
+                 "lng": "25.0001",
+                 "country": "Brazil",
+                 "date": "2020-03-29",
+                 "case_detail": {
+                     "totalCases": 35,
+                     "newCases": 3
+                 }}
+            ],
+            "pagination": {
+                "total_pages": 1,
+                "has_next": False,
+                "has_previous": False,
+                "next_page": None,
+                "current_page": 1
+            }
         })
 
 
